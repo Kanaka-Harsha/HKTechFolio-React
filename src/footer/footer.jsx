29f9943701f6fdd643b0dbb0c5134895554717e8
Kanakadandila Harsha <harshakanakadandila@gmail.com>
2026-02-05 01:14:36 +0530

    Footer Component

---
+++ src/footer/footer.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import './footer.css'
+
+function Footer()
+{
+    return (
+        <div className="footer" id="Footer">
+            <h3>Copyright © | Harsha Kanakadandila | All Rights Reserved</h3>
+        </div>
+    )
+}
+
+export default Footer;
