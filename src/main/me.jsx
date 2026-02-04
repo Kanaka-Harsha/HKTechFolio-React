b91248bad5c22db9a7870052aafe08ce6977acaa
Kanakadandila Harsha <harshakanakadandila@gmail.com>
2026-02-05 01:16:12 +0530

    All UI Components

---
+++ src/main/me.jsx
@@ -0,0 +1,23 @@
+import '../styles/me.css';
+// import myPic from './assets/myPic';
+
+function Me()
+{
+    return (
+        <div className="me" id="Me">
+            <div className="text">
+                <div className="name">
+                    My Name format.
+                </div>
+                <div className="jist">
+                    Here will be linkedin jist
+                </div>
+            </div>
+            <div className="pic">
+                {/* <img src=""></img> */}
+            </div>
+        </div>
+    )
+}
+
+export default Me;
