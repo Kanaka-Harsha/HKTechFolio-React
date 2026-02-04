f5807bdfc6d43126b1af7978d22263bf86bc53f9
Kanakadandila Harsha <harshakanakadandila@gmail.com>
2026-02-05 01:16:51 +0530

    Modified Root

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './header/header'
+import Me from './main/me'
+import Footer from './footer/footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [view, setView] = useState("Home");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header />
+    <div className="container">
+      <Me />
+      {/* <About />
+      <TechStack />
+      <Projects />
+      <ContactMe /> */}
+    </div>
+    <Footer />
     </>
   )
 }
